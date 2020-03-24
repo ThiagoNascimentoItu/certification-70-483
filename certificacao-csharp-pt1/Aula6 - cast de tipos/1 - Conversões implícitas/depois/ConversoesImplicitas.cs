f3d8352f667addaaab068af27d2a447d3b88dfbc
ThiagoNascimentoItu <thiago.nascimento57@etec.sp.gov.br>
2020-03-23 23:01:45 -0300

    Conversões implícitas

---
+++ certificacao-csharp-pt1/Aula6 - cast de tipos/1 - Conversões implícitas/depois/ConversoesImplicitas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ConversoesImplicitas : IAulaItem
+    {
+        public void Executar()
+        {
+            int inteiro = 2_123_456_789;
+            long inteiroLogo = inteiro;
+
+            Console.WriteLine($"Inteiro longo: {inteiroLogo}");
+
+            Gato gato = new Gato();
+            Animal animal = gato;
+
+            IAnimal ianimal = gato;
+
+            Console.WriteLine(ianimal.GetType());
+
+            gato = (Gato)animal;
+
+
+        }
+    }
+}
+
